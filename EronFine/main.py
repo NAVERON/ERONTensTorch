6c5fb6e02dbd5b5993fffc5b2dae522939c843e8
NAVERON <2046783134@qq.com>
2018-12-19 22:34:50 +0800

    ...

--- EronFine/main.py
+++ EronFine/main.py
@@ -7,7 +7,7 @@ from EronFine.evaluator import Evaluator
 from copy import deepcopy
 from EronFine import util
 
-#   在gym环境中欧巴versa体哦那是   3个值
+#   在gym环境中observation体哦那是   3个值
 #   动作是一个值
 #   具体参考      https://www.jianshu.com/p/af3a7853268f
 
